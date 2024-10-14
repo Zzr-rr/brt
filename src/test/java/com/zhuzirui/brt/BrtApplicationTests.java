2b139ded8decb31f89a9132ace00e802844f6def
zhuzirui <zhuzr3210@163.com>
2024-10-14 14:10:01 +0800

    [init]Project init.

---
+++ src/test/java/com/zhuzirui/brt/BrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuzirui.brt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
